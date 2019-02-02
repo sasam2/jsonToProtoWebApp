48f96ef46e2250c6e28a0d09f957e799a9889111
sasam2 <sasam2@workmail.com>
2019-02-02 19:28:28 +0000

    adjustments

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -24,12 +24,19 @@ if [ $? -ne 0 ];then
 echo "Docker run failed."
 exit $?; fi
 
-sleep 6
+sleep 10 
 
 curl -d '{"name":"ivo","id":12}' -H "Content-Type:application/json" -X POST http://localhost:8080/webappassignment/AppServlet
+echo ""
 
 if [ $? -ne 0 ];then 
 echo "Test app failed."; fi
+
+echo ""
+echo "Press key to exit."
+read 
+
+docker logs jsontoproto_webapp
 
 docker container stop jsontoproto_webapp
 
